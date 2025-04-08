62a21b383510083784507043795ef8864f2c889c
Lucas Neto <lucasn-oliveira@hotmail.com>
2025-04-08 20:01:28 -0300

    Fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import dts from "vite-plugin-dts";
+import { libInjectCss } from "vite-plugin-lib-inject-css";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dts(), libInjectCss()],
   build: {
     lib: {
       entry: path.resolve(__dirname, "src/package/index.tsx"),
